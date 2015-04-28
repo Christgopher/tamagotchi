e22e8107e41f464d11bef97b078255b8502f875b
Guest User <student@epicodus.com>
2015-04-28 13:29:59 -0700

    Add and pass Tamagotchi#time_passes spec

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -4,6 +4,8 @@ class Tamagotchi
     @food_level = 10
     @happiness_level = 10
     @sleep_level = 10
+    @initialized_time = Time.new(2015, 4, 28, 0, 0, 0)
+  #  @initialized_time = Time.now()
   end
   define_method(:name) do
     @name
@@ -17,4 +19,12 @@ class Tamagotchi
   define_method(:sleep_level) do
     @sleep_level
   end
+  define_method(:time_passes) do
+    current_time = Time.new(2015, 4, 28, 0, 5, 0)
+    #current_time = Time.now()
+    time_difference = current_time.-(@initialized_time).to_i()
+    levels_to_subtract = time_difference./(300)
+    @food_level = @food_level.-(levels_to_subtract)
+  end
+
 end
